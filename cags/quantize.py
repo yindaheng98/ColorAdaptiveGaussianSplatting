93da194c51b52400de7c0600987d3b4c539859f8
yindaheng98 <yindaheng98@gmail.com>
2025-03-26 13:38:29 -0700

    draco or not

--- cags/quantize.py
+++ cags/quantize.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from gaussian_splatting import GaussianModel
-from cags.quantization import DracoCompressedScalableQuantizer
+from cags.quantization import ScalableQuantizer, DracoCompressedScalableQuantizer
 from scalablevq import n_bits_proposal_balanced_clusters, n_bits_proposal_balanced_values
 
 
@@ -13,14 +13,17 @@ def copy_not_exists(source, destination):
     shutil.copy(source, destination)
 
 
-def quantize(source, destination, iteration, sh_degree, device, quantize, **kwargs):
+def quantize(source, destination, iteration, sh_degree, device, quantize, draco, **kwargs):
     copy_not_exists(os.path.join(source, "cfg_args"), os.path.join(destination, "cfg_args"))
     copy_not_exists(os.path.join(source, "cameras.json"), os.path.join(destination, "cameras.json"))
     input = os.path.join(source, "point_cloud", "iteration_" + str(iteration), "point_cloud.ply")
     output = os.path.join(destination, "point_cloud", "iteration_" + str(iteration), "point_cloud_quantized.ply")
     gaussians = GaussianModel(sh_degree).to(device)
     gaussians.load_ply(input)
-    quantizer = DracoCompressedScalableQuantizer(gaussians, **kwargs)
+    if draco:
+        quantizer = DracoCompressedScalableQuantizer(gaussians, **kwargs)
+    else:
+        quantizer = ScalableQuantizer(gaussians, **kwargs)
     if quantize:
         shutil.rmtree(os.path.join(destination, "point_cloud", "iteration_" + str(iteration)))
         os.makedirs(os.path.join(destination, "point_cloud", "iteration_" + str(iteration)), exist_ok=True)
@@ -40,9 +43,10 @@ if __name__ == "__main__":
     parser.add_argument("--sh_degree", type=int, default=3)
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("-o", "--option", default=[], action='append', type=str)
+    parser.add_argument("--draco", action='store_true')
     parser.add_argument("--dequantize", action='store_true')
     args = parser.parse_args()
     configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option}
     quantize(
         source=args.source, destination=args.destination, iteration=args.iteration, sh_degree=args.sh_degree,
-        device=args.device, quantize=not args.dequantize, **configs)
+        device=args.device, quantize=not args.dequantize, draco=args.draco, **configs)
