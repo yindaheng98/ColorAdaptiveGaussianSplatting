2382a199085122965aaa231ff69b4aa80b5af4c1
yindaheng98 <yindaheng98@gmail.com>
2025-03-27 15:52:36 -0700

    upgrade draco

--- cags/quantization/draco.py
+++ cags/quantization/draco.py
@@ -13,20 +13,19 @@ from .quantizer import ScalableQuantizer
 
 class DracoCompressedScalableQuantizer(ScalableQuantizer):
     def __init__(
-        self, model: GaussianModel,
+        self,
         draco_encoder_executable: str = "./build/Release/draco_encoder.exe" if platform.system() == "Windows" else "./build/draco_encoder",
         draco_decoder_executable: str = "./build/Release/draco_decoder.exe" if platform.system() == "Windows" else "./build/draco_decoder",
         draco_qp=16,
         **kwargs
     ):
-        super().__init__(model, **kwargs)
+        super().__init__(**kwargs)
         self.draco_encoder_executable = draco_encoder_executable
         self.draco_decoder_executable = draco_decoder_executable
         self.draco_qp = draco_qp
         self.draco_q = {}
 
-    def baselayer_ply_dtype(self, layers_dict: Dict[str, List[Layer]]):
-        model = self.model
+    def baselayer_ply_dtype(self, max_sh_degree: int, layers_dict: Dict[str, List[Layer]]):
         force_n_bit_rotation = max(layers_dict['rotation_re'][0].n_bit, layers_dict['rotation_im'][0].n_bit)
         dtype_full = [
             ('x', 'f4'), ('y', 'f4'), ('z', 'f4'),
@@ -38,11 +37,11 @@ class DracoCompressedScalableQuantizer(ScalableQuantizer):
             ('f_dc', self.force_code_dtype or f"u{math.ceil(layers_dict['features_dc'][0].n_bit / 8)}"),
         ]
         force_n_bit_features_rest = 0
-        for sh_degree in range(model.max_sh_degree):
+        for sh_degree in range(max_sh_degree):
             if len(layers_dict[f"features_rest_{sh_degree}"]) <= 0:
                 continue
             force_n_bit_features_rest = max(force_n_bit_features_rest, layers_dict[f'features_rest_{sh_degree}'][0].n_bit)
-        for sh_degree in range(model.max_sh_degree):
+        for sh_degree in range(max_sh_degree):
             force_code_dtype = self.force_code_dtype or f"u{math.ceil(force_n_bit_features_rest / 8)}"
             dtype_full.extend([
                 (f'f_rest_{sh_degree}_0', force_code_dtype),
@@ -56,8 +55,7 @@ class DracoCompressedScalableQuantizer(ScalableQuantizer):
         self.draco_q['features_rest'] = force_n_bit_features_rest
         return dtype_full
 
-    def baselayer_ply_data(self, layers_dict: Dict[str, List[Layer]]):
-        model = self.model
+    def baselayer_ply_data(self, model: GaussianModel, layers_dict: Dict[str, List[Layer]]):
         data_full = [
             *np.array_split(model._xyz.detach().cpu().numpy(), 3, axis=1),
             *np.array_split(torch.zeros_like(model._xyz).detach().cpu().numpy(), 3, axis=1),
@@ -75,9 +73,9 @@ class DracoCompressedScalableQuantizer(ScalableQuantizer):
             data_full.extend(np.array_split(features_rest, 3, axis=1))
         return data_full
 
-    def save_baselayer_ply(self, ply_path: str, layers_dict: Dict[str, List[Layer]]):
-        dtype_full = self.baselayer_ply_dtype(layers_dict)
-        data_full = self.baselayer_ply_data(layers_dict)
+    def save_baselayer_ply(self, model: GaussianModel, ply_path: str, layers_dict: Dict[str, List[Layer]]):
+        dtype_full = self.baselayer_ply_dtype(model.max_sh_degree, layers_dict)
+        data_full = self.baselayer_ply_data(model, layers_dict)
 
         elements = np.rec.fromarrays([data.squeeze(-1) for data in data_full], dtype=dtype_full)
         el = PlyElement.describe(elements, 'vertex')
@@ -96,29 +94,27 @@ class DracoCompressedScalableQuantizer(ScalableQuantizer):
             "-qfeaturerest", str(self.draco_q['features_rest']),
         ])
 
-    def load_baselayer(self, ply_path: str):
+    def load_baselayer(self, max_sh_degree: int, ply_path: str, device):
         drc_path = os.path.splitext(ply_path)[0] + ".drc"
         extract_path = os.path.splitext(ply_path)[0] + ".drcdecode.ply"
         subprocess.check_call([
             self.draco_decoder_executable,
             "-i", drc_path, "-o", extract_path,
         ])
-        model = self.model
         plydata = PlyData.read(extract_path)
         codebooks = np.load(os.path.splitext(ply_path)[0] + ".codebook.npz")
 
         layers_dict = {}
         elements = plydata['vertex']
-        kwargs = dict(dtype=torch.long, device=model._xyz.device)
-        layers_dict["rotation_re"] = [self.load_baselayer_attr("rotation_re", torch.tensor(elements["rot_re"].copy(), **kwargs), codebooks=codebooks)]
-        layers_dict["rotation_im"] = [self.load_baselayer_attr("rotation_im", torch.tensor(elements["rot_im"].copy(), **kwargs), codebooks=codebooks)]
-        layers_dict["opacity"] = [self.load_baselayer_attr("opacity", torch.tensor(elements["opacity"].copy(), **kwargs), codebooks=codebooks)]
-        layers_dict["scaling"] = [self.load_baselayer_attr("scaling", torch.tensor(elements["scale"].copy(), **kwargs), codebooks=codebooks)]
-        layers_dict["features_dc"] = [self.load_baselayer_attr("features_dc", torch.tensor(elements["f_dc"].copy(), **kwargs), codebooks=codebooks)]
-        for sh_degree in range(model.max_sh_degree):
+        layers_dict["rotation_re"] = [self.load_baselayer_attr("rotation_re", torch.tensor(elements["rot_re"].copy(), dtype=torch.int64, device=device), codebooks=codebooks, device=device)]
+        layers_dict["rotation_im"] = [self.load_baselayer_attr("rotation_im", torch.tensor(elements["rot_im"].copy(), dtype=torch.int64, device=device), codebooks=codebooks, device=device)]
+        layers_dict["opacity"] = [self.load_baselayer_attr("opacity", torch.tensor(elements["opacity"].copy(), dtype=torch.int64, device=device), codebooks=codebooks, device=device)]
+        layers_dict["scaling"] = [self.load_baselayer_attr("scaling", torch.tensor(elements["scale"].copy(), dtype=torch.int64, device=device), codebooks=codebooks, device=device)]
+        layers_dict["features_dc"] = [self.load_baselayer_attr("features_dc", torch.tensor(elements["f_dc"].copy(), dtype=torch.int64, device=device), codebooks=codebooks, device=device)]
+        for sh_degree in range(max_sh_degree):
             if not set(f'features_rest_{sh_degree}_{item}' for item in ['codebook', 'cluster_centers', 'n_bits', 'n_leafs']).issubset(codebooks.keys()):
                 layers_dict[f'features_rest_{sh_degree}'] = []
                 continue
-            features_rest = torch.tensor(np.stack([elements[f'f_rest_{sh_degree}_{ch}'] for ch in range(3)], axis=1), **kwargs)
-            layers_dict[f'features_rest_{sh_degree}'] = [self.load_baselayer_attr(f'features_rest_{sh_degree}', features_rest.reshape(-1), codebooks=codebooks)]
+            features_rest = torch.tensor(np.stack([elements[f'f_rest_{sh_degree}_{ch}'] for ch in range(3)], axis=1), dtype=torch.int64, device=device)
+            layers_dict[f'features_rest_{sh_degree}'] = [self.load_baselayer_attr(f'features_rest_{sh_degree}', features_rest.reshape(-1), codebooks=codebooks, device=device)]
         return layers_dict
