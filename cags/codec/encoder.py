515110a61dd485da1581538f01912dfa0add7dee
yindaheng98 <yindaheng98@gmail.com>
2025-03-29 13:39:57 -0700

    better name

--- cags/codec/encoder.py
+++ cags/codec/encoder.py
@@ -12,14 +12,14 @@ class Encoder:
         frame_extractor: InterframeExtractor,
         frame_quantizer: TillingScalableQuantizer,
         tiling_first: bool = True,
-        frame_tiling_rest: AverageSplitTiling = None,
+        tiling_rest: AverageSplitTiling = None,
     ):
         self.frame_extractor = frame_extractor
         self.frame_quantizer = frame_quantizer
-        if frame_tiling_rest is not None:
+        if tiling_rest is not None:
             assert tiling_first, "Tiling first must be set to True to use tiling rest"
         self.tiling_first = tiling_first
-        self.frame_tiling_rest = frame_tiling_rest
+        self.tiling_rest = tiling_rest
 
         self._tile_ids = None
 
@@ -37,8 +37,8 @@ class Encoder:
         if self.tiling_first:
             model = self.frame_quantizer.tiling.sort_as_tiles(model, self._tile_ids)
         diff_gaussians, diff_mask = self.frame_extractor.extract_next(model)
-        if self.frame_tiling_rest is not None:
-            tile_ids = self.frame_tiling_rest.average_split(torch.arange(diff_gaussians._xyz.shape[0], device=diff_gaussians._xyz.device))
+        if self.tiling_rest is not None:
+            tile_ids = self.tiling_rest.average_split(torch.arange(diff_gaussians._xyz.shape[0], device=diff_gaussians._xyz.device))
             layers_dict, tiles, _ = self.frame_quantizer.quantize_tiling(diff_gaussians, update=False, tile_gaussians_ids=tile_ids)
             self.frame_quantizer.save_tiles(ply_path, tiles)
         else:
