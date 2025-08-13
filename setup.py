ed0760ea3574851ad1b15061cc8518ef28fbfe61
yindaheng98 <yindaheng98@gmail.com>
2025-08-13 16:54:44 -0700

    upgrade

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ packages = ['cags'] + ["cags." + package for package in find_packages(where="cag
 
 setup(
     name='cags',
-    version='0.13.0',
+    version='0.14.0',
     author='yindaheng98',
     author_email='yindaheng98@gmail.com',
     url='https://github.com/yindaheng98/cags',
